628f283841a0e3a758ae5745d12c3a326d1976a2
Kishore V <vkishore9701@gmail.com>
2024-03-01 01:49:06 +0530

    Modified the open file by creating a function

--- text_editor_main.c
+++ text_editor_main.c
@@ -1,8 +1,4 @@
-/**
-*@file   text_editor_main.c
-*@brief  Command line text editor using C
-*@author Kishore V (vkishore9701@gmail.com)
-*/
+/* OBJECTIVE : Create a C program to provide a Command Line Text Editor */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,28 +7,56 @@
 #define MAX_FILENAME_LENGTH 20
 #define MAX_BUFFER_SIZE 1024
 
-/**
- * @brief Main Function containing the text editor functionality.
- * @return 0 on successful execution. 
- */
+// A void function to display the menu of operations that can be done in a text editor
+void menu()
+{
+    printf("\n ======Text Editor Menu======\n");
+    printf("1.Open File");
+    printf("\n==============================\n");
+}
+
+// A void function that opens the file if the file exists
+void openFile(char filename[MAX_FILENAME_LENGTH],FILE **file)
+{
+    printf("Enter the filename to open: ");
+    scanf("%s",filename);
+    *file = fopen(filename,"r+");
+    if(*file == NULL)
+    {
+        printf("File not found or could not be opened.\n");
+    }
+    else
+    {
+        printf("File open successful.\n");
+    }
+}
+
+// Main function for command line text editor
 int main()
 {
     char filename[MAX_FILENAME_LENGTH];
     char buffer[MAX_BUFFER_SIZE];
     FILE *file = NULL;
 
-    // Open the file 
-    printf("Enter the filename to open: ");
-    scanf("%s",filename);
-    file = fopen(filename,"r+");
-    if(file == NULL)
-    {
-        printf("File not found or could not be opened.");
-    }
-    else
-    {
-        printf("File open successful.");
-    }
+    // Display the menu
+    menu();
 
+    int choice;
+    printf("Enter your choice: ");
+    scanf("%d",&choice);
+
+    while(1)
+    {
+
+        switch(choice)
+        {
+            case 1:
+            // Open the file 
+            openFile(filename,&file);
+            break;
+
+        }
+
+    }
     return 0;
 }
