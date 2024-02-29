d6b2afbe66e2c0f521091d3647eb37f17cb8585a
Kishore V <vkishore9701@gmail.com>
2024-03-01 00:09:14 +0530

    Created a main file for command line text editor

---
+++ text_editor_main.c
@@ -0,0 +1,15 @@
+/**
+*@file   text_editor_main.c
+*@brief  Command line text editor using C
+*@author Kishore V (vkishore9701@gmail.com)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    printf("Main Function for Command Line Text Editor ");
+    return 0;
+}
