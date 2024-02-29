7cd70376d42004b50d1349bbd4e8e51b0161bb53
Kishore V <vkishore9701@gmail.com>
2024-03-01 01:10:07 +0530

    Added code lines to open a file

--- text_editor_main.c
+++ text_editor_main.c
@@ -8,8 +8,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_FILENAME_LENGTH 20
+#define MAX_BUFFER_SIZE 1024
+
+/**
+ * @brief Main Function containing the text editor functionality.
+ * @return 0 on successful execution. 
+ */
 int main()
 {
-    printf("Main Function for Command Line Text Editor ");
+    char filename[MAX_FILENAME_LENGTH];
+    char buffer[MAX_BUFFER_SIZE];
+    FILE *file = NULL;
+
+    // Open the file 
+    printf("Enter the filename to open: ");
+    scanf("%s",filename);
+    file = fopen(filename,"r+");
+    if(file == NULL)
+    {
+        printf("File not found or could not be opened.");
+    }
+    else
+    {
+        printf("File open successful.");
+    }
+
     return 0;
 }
